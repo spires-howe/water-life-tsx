e8e734ee87792cd3d46ccff8cc959f92fc6da398
kronix129 <nhow741@aucklanduni.ac.nz>
2019-05-15 21:28:49 +1200

    auto scroll to top

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Nav from 'react-bootstrap/Nav';
 import './App.css';
 import { BrowserRouter as Router, Route, Link, Redirect } from "react-router-dom";
 import { NavRouting } from './components/nav-routing/nav-routing';
+import ScrollToTop from 'react-router-scroll-top'
 import Home from './views/home/home';
 import Home2 from './views/home2/home2';
 import About from './views/about/about';
@@ -16,27 +17,29 @@ class App extends React.Component<Props> {
   render() {
     return (
         <Router>
-          <div> 
-            <Navbar bg="light" expand="lg" sticky='top' >
-            <Nav>
-                <Link to="/" style={{fontSize: '2em', fontWeight: 400, textAlign: 'left'}}>
-                <img src={require('./assets/icons/logo.png')} height='60px;'/>
-                Water Life
-                </Link>
-            </Nav>
-              <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-              <Navbar.Collapse>
-              <Nav className="ml-auto">
-                  <NavRouting routePath='/' routeText='Home'/>
-                  <NavRouting routePath='/about' routeText='About'/>
-                  <NavRouting routePath='/pricing' routeText='Products'/>             
-                  <NavRouting routePath='/contact' routeText='Contact'/>                    
+          <ScrollToTop>
+            <div> 
+              <Navbar bg="light" expand="lg" sticky='top' >
+              <Nav>
+                  <Link to="/" style={{fontSize: '2em', fontWeight: 400, textAlign: 'left'}}>
+                  <img src={require('./assets/icons/logo.png')} height='60px;'/>
+                  Water Life
+                  </Link>
               </Nav>
-              </Navbar.Collapse>
-            </Navbar>
-            <Route path="/" exact component={Home} />
-            <Route path="/about/" component={About} />
-          </div>
+                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                <Navbar.Collapse>
+                <Nav className="ml-auto">
+                    <NavRouting routePath='/' routeText='Home'/>
+                    <NavRouting routePath='/about' routeText='About'/>
+                    <NavRouting routePath='/pricing' routeText='Products'/>             
+                    <NavRouting routePath='/contact' routeText='Contact'/>                    
+                </Nav>
+                </Navbar.Collapse>
+              </Navbar>
+              <Route path="/" exact component={Home} />
+              <Route path="/about/" component={About} />
+            </div>
+          </ScrollToTop>
         </Router>
         
     );
