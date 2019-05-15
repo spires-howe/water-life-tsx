bfa47995143de3f079bc9b0af741262094b5519c
kronix129 <nhow741@aucklanduni.ac.nz>
2019-05-15 21:21:14 +1200

    more about page

--- src/views/about/about.tsx
+++ src/views/about/about.tsx
@@ -3,6 +3,9 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import './about.css';
+import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
+import { NavLink } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
 
 export default class About extends React.Component {
     render() {
@@ -30,38 +33,19 @@ export default class About extends React.Component {
                                 <p style={{fontSize: 25}}>
                                 Water Life Limited is a 100% registered and owned company head-quartered in Auckland, New Zealand. We provide the best, latest and most effective measuring, monitoring and analyzing equipment.
                                 </p>
+                                <div className="buttonContainer">
+                                    <ButtonToolbar>
+                                        <NavLink to="/pricing"> 
+                                            <Button variant="outline-dark" style={{fontSize: 20}}>Enquire ➥</Button>
+                                        </NavLink>
+                                    </ButtonToolbar>
+                                </div>
                             </div>
                         </Col>
                     </Row>
-                    <Row>
-                        <Col lg={{offset: 5 , span: 2}}>
-                            <div style={{textAlign: 'center'}}>
-                                <img src={require('../../assets/icons/logo.png')} height="100px"/>
-                            </div>
-                        </Col>
-                    </Row>
-                    <div className="businessContainer">
-                        <Row>
-                            <Col lg={{offset: 4, span: 4}}>
-                                <div style={{marginTop: 30, textAlign: 'center'}}>
-                                    <h2 className="subHeaderText">Business</h2>
-                                    <hr />
-                                </div>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col lg={{offset: 2, span: 8}}>
-                                <div style={{textAlign: 'center'}}>
-                                    <p style={{fontSize: 22}}>
-                                        Water Life Limited’s core business is the supply and maintenance of highly specialised manual, automated, remote-operated, real-time environmental and commercial monitoring equipment.
-                                    </p>
-                                </div>
-                            </Col>
-                        </Row>
-                    </div>
                     <Row>
                         <Col md={{offset: 1, span: 5}} lg={{offset: 2, span: 4}}>
-                            <div style={{textAlign: 'center', marginTop: 50}}>
+                            <div style={{textAlign: 'center', marginTop: 25}}>
                                 <img src={require('../../assets/images/glasses.png')}/>
                                 <p className="aboutAttributeText">
                                     Vision
@@ -72,7 +56,7 @@ export default class About extends React.Component {
                             </div>
                         </Col>
                         <Col md={5} lg={4}>
-                            <div style={{textAlign: 'center', marginTop: 50}}>
+                            <div style={{textAlign: 'center', marginTop: 25}}>
                                 <img src={require('../../assets/images/mission.png')}/>
                                 <p className="aboutAttributeText">
                                     Mission
@@ -85,7 +69,7 @@ export default class About extends React.Component {
                     </Row>
                     <Row>
                         <Col lg={12}>
-                            <div style={{textAlign: 'center', marginTop: 50}}>
+                            <div style={{textAlign: 'center', marginTop: 10}}>
                                 <img src={require('../../assets/images/deal.png')}/>
                                 <p className="aboutAttributeText">
                                     Values
@@ -96,10 +80,140 @@ export default class About extends React.Component {
                             </div>
                         </Col>
                     </Row>
+                    {/* <Row>
+                        <Col lg={{offset: 5 , span: 2}}>
+                            <div style={{textAlign: 'center'}}>
+                                <img src={require('../../assets/icons/logo.png')} height="100px"/>
+                            </div>
+                        </Col>
+                    </Row> */}
+                    <div className="businessContainer">
+                        <Row>
+                            <Col lg={{offset: 2, span: 8}}>
+                                <div style={{marginTop: 50}}>
+                                    <h2 className="subHeaderText">Equipment & Services</h2>
+                                    <hr />
+                                </div>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col lg={{offset: 2, span: 8}}>
+                                <div>
+                                    <p style={{fontSize: 22}}>
+                                        Water Life Limited’s core business is the supply and maintenance of highly specialised manual, automated, remote-operated, real-time environmental and commercial monitoring equipment.
+                                    </p>
+                                </div>
+                                <ButtonToolbar style={{marginBottom: 20}}>
+                                    <NavLink to="/contact"> 
+                                        <Button variant="outline-dark">View More ➥</Button>
+                                    </NavLink>
+                                </ButtonToolbar>
+                            </Col>
+                        </Row>
+                    </div>
+                    <Row>
+                        <Col lg={{offset: 2, span: 4}}>
+                            <div className="priceColumnContainer">
+                                <div className="priceBannerContainer">
+                                    <p style={{marginBottom: 0}} className="bundleText">
+                                        Equipment Provided  
+                                    </p>
+                                </div>
+                                <div className="bundleFeaturesContainer">
+                                    <div className="bundleFeatureContainer">
+                                        <p className="bundleFeatureText">
+                                            - Water Quality Monitoring
+                                        </p>
+                                    </div>
+                                    <div className="bundleFeatureContainer">
+                                        <p className="bundleFeatureText">
+                                            - Air Quality Monitoring/Measurement
+                                        </p>
+                                    </div>
+                                    <div className="bundleFeatureContainer">
+                                        <p className="bundleFeatureText">
+                                            - Occupational Health
+                                        </p>
+                                    </div>
+                                    <div className="bundleFeatureContainer">
+                                        <p className="bundleFeatureText">
+                                            - Scientific and Industrial Equipment
+                                        </p>
+                                    </div>
+                                    <div className="bundleFeatureContainer">
+                                        <p className="bundleFeatureText">
+                                            - Environmental Monitoring
+                                        </p>
+                                    </div>
+                                    <div className="bundleFeatureContainer">
+                                        <p className="bundleFeatureText">
+                                        - Public Health
+                                        </p>
+                                    </div> 
+                                    <div className="bundleFeatureContainer">
+                                        <p className="bundleFeatureText">
+                                        - Food Control
+                                        </p>
+                                    </div> 
+                                    <div className="bundleFeatureContainer">
+                                        <p className="bundleFeatureText">
+                                        - Building/Civil Engineering
+                                        </p>
+                                    </div>    
+                                </div>
+                            </div>
+                        </Col>
+                        <Col lg={4}>
+                            <div className="priceColumnContainer">
+                                <div className="priceBannerContainer">
+                                    <p style={{marginBottom: 0}} className="bundleText">
+                                        Services Provided  
+                                    </p>
+                                </div>
+                                <div className="bundleFeaturesContainer">
+                                    <div className="bundleFeatureContainer">
+                                        <p className="bundleFeatureText">
+                                            - Design/Set Up of Laboratories
+                                        </p>
+                                    </div>
+                                    <div className="bundleFeatureContainer">
+                                        <p className="bundleFeatureText">
+                                            - Water Quality Monitoring Programmes
+                                        </p>
+                                    </div>
+                                    <div className="bundleFeatureContainer">
+                                        <p className="bundleFeatureText">
+                                            - Sanitation Services
+                                        </p>
+                                    </div>
+                                    <div className="bundleFeatureContainer">
+                                        <p className="bundleFeatureText">
+                                            - Scientific and Industrial Equipment
+                                        </p>
+                                    </div>
+                                    <div className="bundleFeatureContainer">
+                                        <p className="bundleFeatureText">
+                                            - Waste Management Services
+                                        </p>
+                                    </div>
+                                    <div className="bundleFeatureContainer">
+                                        <p className="bundleFeatureText">
+                                        - Food Control Programmes
+                                        </p>
+                                    </div> 
+                                    <div className="bundleFeatureContainer">
+                                        <p className="bundleFeatureText">
+                                        - In-House Course Specific Training
+                                        </p>
+                                    </div> 
+                                </div>
+                            </div>
+                        </Col>
+                    </Row>
                     <div style={{marginTop: 50}}>
                         <Row className="aboutBannerContainer">
                             <Col lg={{offset: 1, span: 10}}>
-                                <div style={{marginTop: 100}}>
+                                <div style={{marginTop: 75}}>
                                     <div style={{marginTop: 10}}>
                                         <p className="aboutBannerText">
                                             Strategic Links  
@@ -115,10 +229,66 @@ export default class About extends React.Component {
                             </Col>
                         </Row>
                     </div>
-                    <div style={{marginTop: 150}}>
+                    <div>
+                        <Row>
+                            <Col lg={{offset: 2, span: 8}}>
+                                <div style={{marginTop: 30}}>
+                                    <h2 className="subHeaderText">Current Clients</h2>
+                                    <hr />
+                                </div>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col lg={{offset: 2, span: 8}}>
+                                <ul className="clientList">
+                                    <li style={{fontSize: 22}}>
+                                        Commercial Water Clients
+                                    </li>
+                                    <li style={{fontSize: 22}}>
+                                        Universities and similar Institutions of Higher Learning
+                                    </li>
+                                    <li style={{fontSize: 22}}>
+                                        Environmental Monitoring Companies
+                                    </li>
+                                    <li style={{fontSize: 22}}>
+                                        National Water Quality Monitoring Regulatory Agencies/Departments.
+                                    </li>
+                                    <li style={{fontSize: 22}}>
+                                        Water Bottling Companies
+                                    </li>
+                                    <li style={{fontSize: 22}}>
+                                        Local Authorities – City, District and Municipal Councils
+                                    </li>
+                                    <li style={{fontSize: 22}}>
+                                        Government Occupational Health Departments
+                                    </li>
+                                    <li style={{fontSize: 22}}>
+                                        Public and Private Health Institutions
+                                    </li>
+                                    <li style={{fontSize: 22}}>
+                                        Private Companies
+                                    </li>
+                                    <li style={{fontSize: 22}}>
+                                        Non-governmental Organisations (NGOs)
+                                    </li>
+                                    <li style={{fontSize: 22}}>
+                                        Consultancy Companies
+                                    </li>
+                                    <li style={{fontSize: 22}}>
+                                        Government Ministries and Departments
+                                    </li>
+                                    <li style={{fontSize: 22}}>
+                                        National Environmental Monitoring Regulatory Agencies/Departments.
+                                    </li>
+
+                                </ul>
+                            </Col>
+                        </Row>
+                    </div>
+                    <div style={{marginTop: 20}}>
                         <Row className="aboutBannerContainerSecondary">
                             <Col lg={{offset: 2, span: 8}}>
-                                <div style={{marginTop: 100}}>
+                                <div style={{marginTop: 75}}>
                                     <div style={{marginTop: 10}}>
                                         <p className="aboutBannerText">
                                         Corporate Social Investment (CSl)  
@@ -142,8 +312,7 @@ export default class About extends React.Component {
                             <Col lg={{offset: 2, span: 6}}>
                                 <div>
                                     <h2 className="subHeaderText" style={{marginBottom: 0}}>Management Team</h2>
-                                    <div className="underline">
-                                    </div>
+                                    <hr />
                                     <div style={{marginTop: 10}}>
                                         <p style={{fontSize: 20}}>
                                             <ul>
